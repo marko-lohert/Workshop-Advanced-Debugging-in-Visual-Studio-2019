e8d63e10c7a23144c3db6db93e08bc7766a59f7a
Marko Lohert <lohert.marko@gmail.com>
2021-03-23 22:25:58 +0100

    Clean up code in EmployeeInfo.razor.cs

--- Demo/PeopleSharp/PeopleSharp/Client/Pages/EmployeeInfo.razor.cs
+++ Demo/PeopleSharp/PeopleSharp/Client/Pages/EmployeeInfo.razor.cs
@@ -32,20 +32,9 @@ namespace PeopleSharp.Client.Pages
             {
                 IsSelectedEmployeeChanged = true;
 
-                // Use with to create a new employee record based on selected employee, but with an updated number of vacation days.
-                // In the talk, show equality and reference equality in the case of records.
                 int oldVacationDays = SelectedEmployee.VacationDays;
-
                 Employee newEmployee = SelectedEmployee with { VacationDays = newVacationDays };
-
-                bool equalNewAndSelectedEmployee = newEmployee.Equals(SelectedEmployee);
-                bool referenceEqualNewAndSelectedEmployee = ReferenceEquals(newEmployee, SelectedEmployee);
-
                 Employee newEmployeeOldVacationDays = newEmployee with { VacationDays = oldVacationDays };
-
-                bool equlOldVacationDays = newEmployeeOldVacationDays.Equals(SelectedEmployee);
-                bool referenceEqualOldVacationDays = ReferenceEquals(newEmployeeOldVacationDays, SelectedEmployee);
-
                 SelectedEmployee = newEmployee;
             }
         }
