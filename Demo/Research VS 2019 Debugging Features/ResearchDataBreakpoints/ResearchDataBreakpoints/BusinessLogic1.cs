27b6a553a3488b17f84259f14971c0e75acafc12
Marko Lohert <lohert.marko@gmail.com>
2021-03-24 23:56:32 +0100

    Initial version of app used to demonstrated data breakpoints in Visual Studio.

---
+++ Demo/Research VS 2019 Debugging Features/ResearchDataBreakpoints/ResearchDataBreakpoints/BusinessLogic1.cs
@@ -0,0 +1,22 @@
+﻿namespace ResearchDataBreakpoints
+{
+    class BusinessLogic1
+    {
+        public void Method1(Employee employee1, Employee employee2, Employee employee3)
+        {
+            employee1.FirstName = "Jake";
+            Method2(employee1, employee2, employee3);
+            Method3(employee1, employee2, employee3);
+        }
+
+        public void Method2(Employee employee1, Employee employee2, Employee employee3)
+        {
+            employee3.FirstName = "James";
+        }
+
+        public void Method3(Employee employee1, Employee employee2, Employee employee3)
+        {
+            employee1.FirstName = "Robert";
+        }
+    }
+}
---
+++ Demo/Research VS 2019 Debugging Features/ResearchDataBreakpoints/ResearchDataBreakpoints/BusinessLogic2.cs
@@ -0,0 +1,15 @@
+﻿namespace ResearchDataBreakpoints
+{
+    class BusinessLogic2
+    {
+        public void Method4(Employee employee1, Employee employee2, Employee employee3)
+        {
+            employee1.FirstName = "Jake";
+        }
+
+        public void Method5(Employee employee1, Employee employee2, Employee employee3)
+        {
+            employee2.FirstName = "Kelly";
+        }
+    }
+}
