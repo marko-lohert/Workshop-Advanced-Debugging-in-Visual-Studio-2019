9e38c0ca8fda96168a50d05eed05e77a5b794ab9
Marko Lohert <lohert.marko@gmail.com>
2021-03-29 23:08:17 +0200

    ResearchThreadsDebuggingSupport - Initial version of app used to demonstrate how to debug applications that use threads

---
+++ Demo/Research VS 2019 Debugging Features/ResearchThreadsDebuggingSupport/ResearchThreadsDebuggingSupport/TestMethods.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads
+{
+    public class TestMethods
+    {
+        private Random Rnd { get; init; } = new();
+        private const int minRnd = 100;
+        private const int maxRnd = 1000;
+
+        public void MethodA()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"MethodA: i = {i}");
+                Thread.Sleep(Rnd.Next(minRnd, maxRnd));
+            }
+        }
+
+        public void MethodB()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"MethodB: i = {i}");
+                Thread.Sleep(Rnd.Next(minRnd, maxRnd));
+            }
+        }
+
+        public void MethodC()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"MethodC: i = {i}");
+                Thread.Sleep(Rnd.Next(minRnd, maxRnd));
+                
+                MethodC1();
+            }
+        }
+
+        public void MethodC1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"MethodC1: i = {i}");
+                Thread.Sleep(Rnd.Next(minRnd, maxRnd));
+
+                MethodC2();
+            }
+        }
+
+        public void MethodC2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"MethodC2: i = {i}");
+                Thread.Sleep(Rnd.Next(minRnd, maxRnd));
+            }
+        }
+    }
+}
